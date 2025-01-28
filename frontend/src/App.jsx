e8c631165f88944aaf5faae142791257393203ff
Ali Ilyas <alinoob295@gmail.com>
2025-01-28 12:42:10 +0500

    dashboards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,30 @@ import { BrowserRouter, Routes , Route } from "react-router-dom"
 import RegisterPage from "./Pages/RegisterPage"
 import { ToastContainer} from 'react-toastify';
 import LoginPage from "./Pages/LoginPage";
+import HomePage from "./Pages/HomePage";
+import DashboardLayout from "./Pages/Admin/DashboardLayout";
+import Dashboard from "./Pages/Admin/Dashboard";
+import Users from "./Pages/Admin/Users";
+import Products from "./Pages/Admin/Products";
+import Orders from "./Pages/Admin/Orders";
+
 
 function App() {
  return (
     <>
    <BrowserRouter>
    <Routes>
+
+  <Route path="/" element={<HomePage/>}/>
+
+  <Route path="/admin" element={<DashboardLayout/>}>
+  <Route index element={<Dashboard/>}/>
+  <Route path="Users" element={<Users/>}/>
+  <Route path="Products" element={<Products/>}/>
+  <Route path="Orders" element={<Orders/>}/>
+ 
+  </Route>
+  
   <Route path="/register" element={<RegisterPage/>}/>
   <Route path="/login" element={<LoginPage/>}/>
    </Routes>
